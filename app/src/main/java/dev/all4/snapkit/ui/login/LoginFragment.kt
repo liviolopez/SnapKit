f92e66274e5e65e98d90a43247972e1d542fa21f
Livio Lopez <liviolopez@gmail.com>
2020-12-04 02:54:24 -0500

    added basic SnapChat login button to LoginFragment.kt

--- app/src/main/java/dev/all4/snapkit/ui/login/LoginFragment.kt
+++ app/src/main/java/dev/all4/snapkit/ui/login/LoginFragment.kt
@@ -5,6 +5,8 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import com.snapchat.kit.sdk.SnapLogin
+import com.snapchat.kit.sdk.core.controller.LoginStateController.OnLoginStateChangedListener
 import dev.all4.snapkit.R
 import dev.all4.snapkit.databinding.FragmentLoginBinding
 
@@ -20,5 +22,21 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         loginViewModel.text.observe(viewLifecycleOwner, Observer {
             binding.textLogin.text = it
         })
+
+        setupSnapLogin()
+    }
+
+    private fun setupSnapLogin(){
+        SnapLogin.getButton(requireContext(), binding.snapLoginButtonContainer)
+
+        val onLoginStateChangedListener = object : OnLoginStateChangedListener {
+            override fun onLoginSucceeded() { }
+
+            override fun onLoginFailed() { }
+
+            override fun onLogout() { }
+        }
+
+        SnapLogin.getLoginStateController(requireContext()).addOnLoginStateChangedListener(onLoginStateChangedListener)
     }
 }
